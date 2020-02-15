8976fc01c085b6add9a2976896b0b91fe4a975b8
Jakob Fix <jakob.fix@gmail.com>
2020-02-16 00:15:53 +0100

    fdd

--- functions/api.js
+++ functions/api.js
@@ -3,21 +3,29 @@
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
-exports.handler = (event, context, callback) => {
-  const url = `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-  const client = new MongoClient(url, { useUnifiedTopology: true })
-  client.connect()
-    .then((client) => {
-      console.log("Connected successfully to server")
-      const db = client.db(process.env.MONGO_DB)
-      client.close()
 
-      callback(null, {
-        statusCode: 200,
-        body: 'No worries, all is working fine!'
-      })    
+exports.handler = (event, context, callback) => {
+  try {
+    const url = `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/test?retryWrites=true&w=majority`
+    const client = new MongoClient(url, { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true 
     })
-    .catch((err) => {
+    client.connect()
+      .then((client) => {
+        console.log("Connected successfully to server")
+        const db = client.db(process.env.MONGO_DB)
+        client.close()
+  
+        callback(null, {
+          statusCode: 200,
+          body: 'No worries, all is working fine!'
+        })    
+      })
+      .catch((err) => {
+        callback(err)
+      })
+    } catch(e) {
       callback(err)
-    })
+  }
 }
