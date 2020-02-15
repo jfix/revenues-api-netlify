5a63fcaa8e682cf4ab2c52ab2316f5b33a7218de
Jakob Fix <jakob.fix@gmail.com>
2020-02-15 23:49:03 +0100

    blah

--- functions/api.js
+++ functions/api.js
@@ -3,17 +3,16 @@
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
-const url = `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-const client = new MongoClient(url, { useUnifiedTopology: true })
-
 exports.handler = (event, context, callback) => {
+  const url = `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  const client = new MongoClient(url, { useUnifiedTopology: true })
   client.connect()
     .then((client) => {
       console.log("Connected successfully to server")
       const db = client.db(process.env.MONGO_DB)
       client.close()
 
-      callback(null, {
+      return callback(null, {
         statusCode: 200,
         body: 'No worries, all is working fine!'
       })    
