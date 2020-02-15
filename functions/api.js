a1e82f99ca07d533d8a4ace2058d5d3045956dd0
Jakob Fix <jakob.fix@gmail.com>
2020-02-15 23:37:14 +0100

    try to connect to db

--- functions/api.js
+++ functions/api.js
@@ -1,11 +1,14 @@
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
+'use strict';
+
+const MongoClient = require('mongodb').MongoClient({ useUnifiedTopology: true })
 require('dotenv').config()
 
 const url = `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useUnifiedTopology: true })
+
 exports.handler = (event, context, callback) => {
-  MongoClient.connect(url, function (err, client) {
-    assert.equal(null, err);
+  client.connect(url, function (err, client) {
+    if (err) throw err
     console.log("Connected successfully to server")
     const db = client.db(process.env.MONGO_DB)
     client.close()
