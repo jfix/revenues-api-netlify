75fd3b82434f4e340b0e7637c9aaa6a1c236a26c
Jakob Fix <jakob.fix@gmail.com>
2020-02-15 17:40:24 +0100

    Add Mongo connector

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,17 @@
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+require('dotenv').config()
+
+const url = `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 exports.handler = (event, context, callback) => {
-    callback(null, {
-      statusCode: 200,
-      body: 'No worries, all is working fine!'
-    })
+  MongoClient.connect(url, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server")
+    const db = client.db(process.env.MONGO_DB)
+    client.close()
+  });
+  callback(null, {
+    statusCode: 200,
+    body: 'No worries, all is working fine!'
+  })
 }
